793064fceb46cf59359997de5e453a4a7ffd1473
C0rpsy <2065665hoof@zuyd.nl>
2024-12-04 16:51:55 +0100

    .

---
+++ InleverOpdracht/Classes/User.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InleverOpdracht.Classes
+{
+    internal class User
+    {
+        public int Id { get; set; }
+        public string Username { get; set; }
+        public string PasswordHash { get; set; } // Store hashed passwords for security
+    }
+}
