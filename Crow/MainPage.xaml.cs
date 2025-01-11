89f8e745c654996e68fa47af02bcd03d3f9bdef6
C0rpsy <2065665hoof@zuyd.nl>
2025-01-11 13:47:00 +0100

    All errors gone for now
    Refactor event handlers and update namespaces
    
    Removed Clicked event handlers from LoginPage.xaml and RegisterPage.xaml. Corrected Statusmessage binding and added xmlns:vm namespace in RegisterPage.xaml. Updated xmlns:vm namespace in ThemePage.xaml. Fixed namespace declaration and removed unused variable in MainPage.xaml.cs. Added navigation methods for LoginPage and RegisterPage.

--- Crow/MainPage.xaml.cs
+++ Crow/MainPage.xaml.cs
@@ -1,14 +1,27 @@
-﻿namespace Crow
+﻿using Crow.MVVM.Views;
+
+namespace Crow
 {
     public partial class MainPage : ContentPage
     {
-        int count = 0;
-
         public MainPage()
         {
             InitializeComponent();
         }
 
+        private async void OnLoginButtonClicked(object sender, EventArgs e)
+        {
+            // Navigate to LoginPage
+            await Navigation.PushAsync(new LoginPage());
+
+        }
+
+        private async void OnRegisterButtonClicked(object sender, EventArgs e)
+        {
+            // Navigate to RegisterPage
+            await Navigation.PushAsync(new RegisterPage());
+        }
     }
 
 }
+
